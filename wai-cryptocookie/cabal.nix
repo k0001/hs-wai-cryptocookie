4676fac4a7ba6c64e872e8f527a1dfcb11590e70
Renzo Carbonara <gnuk0001@gmail.com>
2025-11-24 11:19:12 +0000

    verbump

--- wai-cryptocookie/cabal.nix
+++ wai-cryptocookie/cabal.nix
@@ -1,17 +1,18 @@
-{ mkDerivation, aeson, base, bytestring, case-insensitive, cookie
-, crypton, directory, filepath, http-types, lib, memory, text, time
-, wai, wai-csrf, wai-extra
+{ mkDerivation, aeson, base, binary, bytestring, case-insensitive
+, cookie, crypton, directory, filepath, http-types, lib, memory
+, text, time, wai, wai-csrf, wai-extra
 }:
 mkDerivation {
   pname = "wai-cryptocookie";
-  version = "0.3";
+  version = "0.3.1";
   src = ./.;
   libraryHaskellDepends = [
-    aeson base bytestring case-insensitive cookie crypton http-types
-    memory text time wai wai-csrf
+    aeson base binary bytestring case-insensitive cookie crypton
+    http-types memory text time wai wai-csrf
   ];
   testHaskellDepends = [
-    base bytestring cookie directory filepath http-types wai wai-extra
+    aeson base binary bytestring cookie directory filepath http-types
+    text wai wai-extra
   ];
   homepage = "https://github.com/k0001/hs-wai-cryptocookie";
   description = "Encrypted cookies for WAI";
